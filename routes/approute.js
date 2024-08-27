5d23da456b139a7f7c0ed07b688d7239f1f32210
Prakash Singh Basnal <72181041+Prakash0Singh@users.noreply.github.com>
2024-08-27 09:22:17 +0000

    new-route

--- routes/approute.js
+++ routes/approute.js
@@ -1,18 +1,12 @@
-// approute.js
 const express = require('express');
 const router = express.Router();
 
-// Middleware to parse JSON bodies
 router.use(express.json());
 
 router.post('/submit-form', (req, res) => {
     const formData = req.body;
-    
-    console.log("custom")
-    // Process formData as needed
     console.log('Form data received:', formData);
 
-    // Send a response back to the client
     res.json({ message: 'Form submitted successfully!' });
 });
 
