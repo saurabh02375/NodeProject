f7c5e7df4edf0d01b3670ca9d854cb6045684084
Prakash Singh Basnal <72181041+Prakash0Singh@users.noreply.github.com>
2024-08-29 05:01:10 +0000

    default-database-change

--- database/database.js
+++ database/database.js
@@ -1,21 +1,21 @@
 const sqlite3 = require("sqlite3").verbose();
 const path = require("path");
 
-// Define the path to the database file
 const dbPath = path.resolve(__dirname, "mydatabase.db");
 
-// Open the SQLite database
 const db = new sqlite3.Database(dbPath, (err) => {
   if (err) {
     console.error("Error opening database:", err.message);
   } else {
     console.log("Connected to the SQLite database.");
 
-    // Create the users table if it does not exist
     db.run(
       `CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        username TEXT UNIQUE NOT NULL
+        username TEXT UNIQUE NOT NULL,
+        email TEXT UNIQUE NOT NULL,
+        number NUMBER	 UNIQUE NOT NULL,
+        password TEXT NOT NULL,
       )`,
       (err) => {
         if (err) {
